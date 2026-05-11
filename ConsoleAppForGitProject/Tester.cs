6c1fa5fe0cb80e3c179d737e4947d0a5ad9a6786
nehagonde23-dotcom <nehagonde23@gmail.com>
2026-05-11 11:43:50 +0530

    Added File in the tester branch

---
+++ ConsoleAppForGitProject/Tester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForGitProject
+{
+    internal class Tester
+    {
+        public void AddTesterData()
+        {
+            Console.WriteLine("Calling Tester Data");
+        }
+    }
+}
