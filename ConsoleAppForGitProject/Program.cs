5535b725cde2ff211c862e4109ec0c36f1a6f182
nehagonde23-dotcom <nehagonde23@gmail.com>
2026-05-11 11:38:35 +0530

    Add project files.

---
+++ ConsoleAppForGitProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForGit
+{
+	internal class Program
+	{
+		void Addition()
+		{
+			int a = 10, b = 20, c;
+			c = a + b;
+			Console.WriteLine($"Addition ={c}");
+		}
+		static void Main(string[] args)
+		{
+			Program p = new Program();
+			p.Addition();
+		}
+	}
+}
